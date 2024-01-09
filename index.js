cd261937ce2a5b70ba1215d46ab4e47a8e885fe1
Biparker <bparker873@gmail.com>
2024-01-09 11:40:39 -0500

    Update index.js 3

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ const server = http.createServer((req, res) => {
   res.end('Hello World!');
 });
 
+app.get('/fetch-secret', async (req, res) => {
+    axios.get('https://nodejs-production-beb2.up.railway.internal:3000/secret')
+    .then(response => {
+        res.json(response.data);
+    })
+})
 
 server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
